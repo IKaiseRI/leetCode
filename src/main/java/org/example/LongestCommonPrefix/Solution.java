bc58c898f7d3c3287f494956679fc3e776405476
Ion Vornovitchi <ariathvs@gmail.com>
2024-04-03 13:38:52 +0300

    Longest common prefix solution

---
+++ src/main/java/org/example/LongestCommonPrefix/Solution.java
@@ -0,0 +1,28 @@
+package org.example.LongestCommonPrefix;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String longestCommonPrefix(String[] strings) {
+        return strings == null || strings.length == 0 ? "" : Arrays.stream(strings)
+                .reduce((s1, s2) -> {
+                    int length = Math.min(s1.length(), s2.length());
+                    int index = 0;
+                    while (index < length && s1.charAt(index) == s2.charAt(index)) {
+                        index++;
+                    }
+                    return s1.substring(0, index);
+                })
+                .orElse("");
+    }
+
+    public static String findPrefix(String s1, String s2) {
+
+    }
+}
