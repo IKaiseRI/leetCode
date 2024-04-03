60e8d8fac97306b445a20ea3ae722418144bb15e
Ion Vornovitchi <ariathvs@gmail.com>
2024-04-03 12:00:53 +0300

    Integer to Roman solution

---
+++ src/main/java/org/example/IntegerToRoman/Solution.java
@@ -0,0 +1,18 @@
+package org.example.IntegerToRoman;
+
+import java.util.Map;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        intToRoman(1994);
+    }
+
+    public static String intToRoman(int num) {
+        String M[] = {"", "M", "MM", "MMM"};
+        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
+}
